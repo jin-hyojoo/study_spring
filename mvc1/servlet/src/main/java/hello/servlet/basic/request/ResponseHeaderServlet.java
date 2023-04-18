ee011e5c8d5e89fe58efe41ad92a20aed4b1263d
jin-hyojoo <cuteseal97@gmail.com>
2023-04-18 17:58:25 +0900

    [section.2] Http Request Data(API message body), HttpServletResponse

---
+++ mvc1/servlet/src/main/java/hello/servlet/basic/request/ResponseHeaderServlet.java
@@ -0,0 +1,64 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // [status-line]
+        response.setStatus(HttpServletResponse.SC_OK); // HTTP 응답코드
+
+        // [response-headers]
+        response.setHeader("Content-Type", "text/plain;charset=utf8"); // 한글사용 ok
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello im Nyozu");
+
+        // [Header 편의 method]  1.content  2.cookie  3.redirect
+        //content(response);
+        //cookie(response);
+        redirect(response);
+
+
+        // [message body]
+        PrintWriter writer = response.getWriter();
+        writer.println("리스폰스 객체 예제");
+    }
+
+    // 1. 컨텐츠 편의 method
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    // 2. 쿠키 편의 method
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;  // 해당 쿠키는 600초 동안 유효하다
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    // 3. 리다이렉트 편의 method
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html"); // 해당 로케이션 재호출
+        response.sendRedirect("/basic/hello-form.html");
+    }
+}
