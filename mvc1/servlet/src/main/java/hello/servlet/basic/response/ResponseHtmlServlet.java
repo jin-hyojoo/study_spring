f6e5272e8a21220d1e671e0f3245a0b4cf72296c
jin-hyojoo <cuteseal97@gmail.com>
2023-04-19 17:55:39 +0900

    [section.2] Http Response Data

---
+++ mvc1/servlet/src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Content-type : text/html; charset=utf-8
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.print("<html>");
+        writer.print("<body>");
+        writer.print("<div> 하이루 방가방가 </div>");
+        writer.print("</body>");
+        writer.print("</html>");
+    }
+}
